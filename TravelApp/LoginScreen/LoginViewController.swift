6249df24de5841c5252a1a845870f7efaf58225f
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-09-07 17:47:39 +0300

    Kadirhan Commit

--- TravelApp/LoginScreen/LoginViewController.swift
+++ TravelApp/LoginScreen/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //deneme satırı Kadirhan
     }
 
 
